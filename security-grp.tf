ad1393e1a1933c1c22400700a90ae9d27a78429b
rjdevnic <RjDev@users.noreply.github.com>
2022-10-05 12:39:20 +0530

    Update security-grp.tf

--- security-grp.tf
+++ security-grp.tf
@@ -14,38 +14,42 @@ resource "aws_security_group" "public" {
  
 resource "aws_security_group_rule" "public_out" {
   type        = "egress"
+  description = "Public internet access"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["10.0.0.0/32"]
  
   security_group_id = aws_security_group.public.id
 }
  
 resource "aws_security_group_rule" "public_in_ssh" {
   type              = "ingress"
+  description = "Public internet access"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["10.0.0.0/32"]
   security_group_id = aws_security_group.public.id
 }
  
 resource "aws_security_group_rule" "public_in_http" {
   type              = "ingress"
+  description = "Public internet access"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["10.0.0.0/32"]
   security_group_id = aws_security_group.public.id
 }
  
 resource "aws_security_group_rule" "public_in_https" {
   type              = "ingress"
+  description = "Public internet access"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["10.0.0.0/32"]
   security_group_id = aws_security_group.public.id
 }
  
@@ -69,20 +73,22 @@ resource "aws_security_group" "private" {
  
 resource "aws_security_group_rule" "private_out" {
   type        = "egress"
+  description = "Public internet access"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["10.0.0.0/32"]
  
   security_group_id = aws_security_group.private.id
 }
  
 resource "aws_security_group_rule" "private_in" {
   type              = "ingress"
+  description = "Public internet access"
   from_port         = 0
   to_port           = 65535
   protocol          = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["10.0.0.0/32"]
  
   security_group_id = aws_security_group.private.id
 }
